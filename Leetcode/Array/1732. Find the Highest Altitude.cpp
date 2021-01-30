b6ab93e4c69bdce5aa3ed2f3c676072a31091ec5
WasimAkhtarKhan <50516723+WasimAkhtarKhan@users.noreply.github.com>
2021-01-30 11:29:03 +0530

    Create 1732. Find the Highest Altitude.cpp

---
+++ Leetcode/Array/1732. Find the Highest Altitude.cpp
@@ -0,0 +1,18 @@
+//https://leetcode.com/problems/find-the-highest-altitude/
+
+
+
+class Solution {
+public:
+    int largestAltitude(vector<int>& gain) {
+        int highest=0;
+        int curr=0;
+        for(int i=0;i<gain.size();i++)
+        {
+            curr += gain[i];
+            if(curr>highest)
+                highest = curr;
+        }
+        return highest;
+    }
+};
