cc7eff837c09e2d0005363adea1bc0555ad78545
WasimAkhtarKhan <50516723+WasimAkhtarKhan@users.noreply.github.com>
2021-02-01 19:49:43 +0530

    Update Number of 1 Bits.cpp

--- Leetcode/February LeetCoding Challenge 2021/Number of 1 Bits.cpp
+++ Leetcode/February LeetCoding Challenge 2021/Number of 1 Bits.cpp
@@ -1,1 +1,16 @@
-sc
+//https://leetcode.com/explore/featured/card/february-leetcoding-challenge-2021/584/week-1-february-1st-february-7th/3625/
+
+
+
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        unsigned int count = 0;
+        while (n > 0) {     
+            if ((n & 1) == 1)
+                count++;
+            n >>= 1;
+        }
+        return count;
+    }
+};
