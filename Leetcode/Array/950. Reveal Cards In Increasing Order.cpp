34ad5a63e19610e2bd8be14745576854af6d7f98
WasimAkhtarKhan <50516723+WasimAkhtarKhan@users.noreply.github.com>
2021-02-14 07:11:19 +0530

    Update 950. Reveal Cards In Increasing Order.cpp

--- Leetcode/Array/950. Reveal Cards In Increasing Order.cpp
+++ Leetcode/Array/950. Reveal Cards In Increasing Order.cpp
@@ -1,6 +1,4 @@
 //https://leetcode.com/problems/reveal-cards-in-increasing-order/
-
-
 class Solution {
 public:
     vector<int> deckRevealedIncreasing(vector<int>& deck) {
@@ -10,22 +8,20 @@ public:
         
         deque<int>de;
         sort(deck.begin(),deck.end());
-        int a=deck.back();
-        de.push_front(a);
-        deck.pop_back();
-        n=n-2;
         while(n)
-        {
+        {    
             int a=deck.back();
             de.push_front(a);
             deck.pop_back();
-            int j=de.back();
-            de.pop_back();
-            de.push_front(j);
+            
+            if(n!=1)
+            {
+                int j=de.back();
+                de.pop_back();
+                de.push_front(j);
+            }
             n--;
         }
-        a=deck.back();
-        de.push_front(a);
         vector<int> ans(de.begin(), de.end());
         return ans;
     }
