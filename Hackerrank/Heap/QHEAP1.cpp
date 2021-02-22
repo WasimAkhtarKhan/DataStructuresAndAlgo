44a256b54af1704d7a818c0a672abf16bae6fcf9
WasimAkhtarKhan <50516723+WasimAkhtarKhan@users.noreply.github.com>
2021-02-22 23:33:55 +0530

    Create QHEAP1.cpp

---
+++ Hackerrank/Heap/QHEAP1.cpp
@@ -0,0 +1,25 @@
+//https://www.hackerrank.com/challenges/qheap1/problem
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <set>
+using namespace std;
+
+int main() {
+    int q, x, y;
+    cin >> q;
+    multiset<int> pq;
+    while (q-- > 0) {
+        cin >> x;
+        if (x == 1)
+            cin >> y, pq.insert(y);
+        else if (x == 2)
+            cin >> y, pq.erase(y);
+        else
+            cout << *pq.begin() << endl;
+    }
+    return 0;
+}
