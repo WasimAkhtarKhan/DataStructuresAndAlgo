0f67167f46eddb0befc89fbf02cc65bce7262459
WasimAkhtarKhan <50516723+WasimAkhtarKhan@users.noreply.github.com>
2021-03-12 23:28:33 +0530

    Update Tree: Preorder Traversal.cpp

--- Hackerrank/Trees/Tree: Preorder Traversal.cpp
+++ Hackerrank/Trees/Tree: Preorder Traversal.cpp
@@ -1,25 +1,6 @@
 //https://www.hackerrank.com/challenges/tree-preorder-traversal/problem
 
 
-
-
-/* you only have to complete the function given below.  
-Node is defined as  
-
-class Node {
-    public:
-        int data;
-        Node *left;
-        Node *right;
-        Node(int d) {
-            data = d;
-            left = NULL;
-            right = NULL;
-        }
-};
-
-*/
-
     void preOrder(Node *root) {
 
         if(root!=NULL)
