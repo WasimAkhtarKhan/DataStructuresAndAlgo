032e1abf335578fadebf9b0c257e311a034c8ac6
WasimAkhtarKhan <50516723+WasimAkhtarKhan@users.noreply.github.com>
2021-05-31 17:59:29 -0700

    Update Find row with maximum no. of 1's.cpp

--- QuesByBabbarFINAL450/Find row with maximum no. of 1's.cpp
+++ QuesByBabbarFINAL450/Find row with maximum no. of 1's.cpp
@@ -1,5 +1,32 @@
 //https://practice.geeksforgeeks.org/problems/row-with-max-1s0023/1#
+//Method 1:O(N+M)
 
+class Solution{
+public:
+    int rowWithMax1s(vector<vector<int> > arr, int n, int m) {
+        int r = 0;              // keeps track of row; starts at first row             
+        int c = m-1;            // keeps track of column; starts at last column
+        int max_row_index=-1;   // keeps track of result row index
+        
+        // starting from top right corner
+        // go left if you encounter 1
+        // do down if you encounter 0
+        while(r<n && c>=0)
+        {
+            if(arr[r][c]==1)
+            {
+                max_row_index = r;
+                c--;
+            }
+            else
+                r++;
+        }
+        return max_row_index;
+    }
+};
+
+
+//Method 2:O(N*M)
 class Solution{
 public:
 	int rowWithMax1s(vector<vector<int> > arr, int n, int m) {
