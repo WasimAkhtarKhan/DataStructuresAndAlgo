c1c735b44735405709de44645dcd836862ef4e8f
WasimAkhtarKhan <50516723+WasimAkhtarKhan@users.noreply.github.com>
2021-06-17 15:17:51 +0530

    Create Reverse a String.cpp

---
+++ QuesByBabbarFINAL450/Reverse a String.cpp
@@ -0,0 +1,14 @@
+//https://leetcode.com/problems/reverse-string/
+
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int temp;
+        for(int i=0;i<s.size()/2;i++)
+        {
+            temp=s[i];
+            s[i]=s[s.size()-i-1];
+            s[s.size()-i-1]=temp;
+        }
+    }
+};
