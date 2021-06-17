22be1e1d3a418eb3c8275ddb580fa9e7ac3ec8ba
WasimAkhtarKhan <50516723+WasimAkhtarKhan@users.noreply.github.com>
2021-06-17 16:07:53 +0530

    Create Print all the duplicates in the input string.cpp

---
+++ QuesByBabbarFINAL450/Print all the duplicates in the input string.cpp
@@ -0,0 +1,19 @@
+//https://www.geeksforgeeks.org/print-all-the-duplicates-in-the-input-string/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string str = "test string";
+    map<char, int> count;
+    for (int i = 0; i < str.length(); i++) {
+        count[str[i]]++;
+    }
+ 
+    for (auto it : count) {
+        if (it.second > 1)
+            cout << it.first << ", count = " << it.second << "\n";
+    }
+    return 0;
+}
