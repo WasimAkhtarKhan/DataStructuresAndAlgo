448dbdc06783407aeac4fd1017aac6e536c99299
WasimAkhtarKhan <50516723+WasimAkhtarKhan@users.noreply.github.com>
2021-08-28 22:42:59 +0530

    Update Recursively remove all adjacent duplicates.cpp

--- QuesByBabbarFINAL450/Recursively remove all adjacent duplicates.cpp
+++ QuesByBabbarFINAL450/Recursively remove all adjacent duplicates.cpp
@@ -1,5 +1,28 @@
 //https://practice.geeksforgeeks.org/problems/consecutive-elements2306/1
 
+//recursive solution
+class Solution{
+    public:
+    void rem(string &S,int l)
+    {
+        if(l==0)
+        return;
+        
+        if(S[l-1]==S[l-2])
+        S.erase(S.begin()+l-1);
+        
+        rem(S,l-1);
+    }
+    
+    string removeConsecutiveCharacter(string S)
+    {
+        // code here.
+        rem(S,S.length());
+        return S;
+    }
+};
+
+//Another Solution
 class Solution{
     public:
     string removeConsecutiveCharacter(string S)
