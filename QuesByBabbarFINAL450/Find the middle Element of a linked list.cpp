133354815654935f03b90288daa0d4e84816de6e
WasimAkhtarKhan <50516723+WasimAkhtarKhan@users.noreply.github.com>
2021-10-24 04:56:14 +0530

    Create Find the middle Element of a linked list.cpp

---
+++ QuesByBabbarFINAL450/Find the middle Element of a linked list.cpp
@@ -0,0 +1,23 @@
+//https://leetcode.com/problems/middle-of-the-linked-list/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* s = head;
+        while(head!=NULL&&head->next!=NULL){
+            head=head->next->next;
+            s=s->next;
+        }
+        return s;
+    }
+};
