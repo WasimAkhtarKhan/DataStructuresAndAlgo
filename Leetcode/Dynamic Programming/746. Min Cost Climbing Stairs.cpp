afe397dc636f3f898b69fd2e945d727b51928792
WasimAkhtarKhan <50516723+WasimAkhtarKhan@users.noreply.github.com>
2022-01-07 22:38:04 +0530

    Create 746. Min Cost Climbing Stairs.cpp

---
+++ Leetcode/Dynamic Programming/746. Min Cost Climbing Stairs.cpp
@@ -0,0 +1,16 @@
+//https://leetcode.com/problems/min-cost-climbing-stairs/
+
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        int n = cost.size();
+        int arr[n];
+        arr[n-1] = cost[n-1];
+        arr[n-2] = cost[n-2];
+        
+        for(int i=n-3;i>=0;i--){
+            arr[i] = cost[i] + min(arr[i+1],arr[i+2]);
+        }
+        return min(arr[0],arr[1]);
+    }
+};
