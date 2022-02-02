6143d51fefbdb69e47c1c29d88fc200e914165b0
WasimAkhtarKhan <50516723+WasimAkhtarKhan@users.noreply.github.com>
2022-02-02 10:32:09 +0530

    Create square root of an integer.cpp

---
+++ QuesByBabbarFINAL450/square root of an integer.cpp
@@ -0,0 +1,9 @@
+//https://practice.geeksforgeeks.org/problems/count-squares3649/1#
+
+class Solution {
+  public:
+    int countSquares(int N) {
+        // code here
+        return sqrt(N-1);
+    }
+};
