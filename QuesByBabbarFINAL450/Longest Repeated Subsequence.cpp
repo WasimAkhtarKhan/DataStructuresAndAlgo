d342006d6725f48d86c5664e67c63bb43d27a4eb
WasimAkhtarKhan <50516723+WasimAkhtarKhan@users.noreply.github.com>
2022-03-30 16:23:33 +0530

    Create Longest Repeated Subsequence.cpp

---
+++ QuesByBabbarFINAL450/Longest Repeated Subsequence.cpp
@@ -0,0 +1,18 @@
+//https://practice.geeksforgeeks.org/problems/longest-repeating-subsequence2004/1#
+
+int LongestRepeatingSubsequence(string str){
+		    // Code here
+		    int n = str.length();
+		    int dp[n+1][n+1];
+		    memset(dp,0,sizeof dp);
+		    
+		    for(int i=1;i<=n;i++){
+		        for(int j=1;j<=n;j++){
+		            if(str[i-1]==str[j-1]&&i!=j)
+		            dp[i][j] = 1+dp[i-1][j-1];
+		            else
+		            dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+		        }
+		    }
+		    return dp[n][n];
+		}
