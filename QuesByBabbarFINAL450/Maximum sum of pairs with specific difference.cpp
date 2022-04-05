bcb806fb5a64454b6d0108c388a1d9f5c54b9a57
WasimAkhtarKhan <50516723+WasimAkhtarKhan@users.noreply.github.com>
2022-04-05 13:21:23 +0530

    Create Maximum sum of pairs with specific difference.cpp

---
+++ QuesByBabbarFINAL450/Maximum sum of pairs with specific difference.cpp
@@ -0,0 +1,16 @@
+//https://practice.geeksforgeeks.org/problems/pairs-with-specific-difference1533/1
+
+int maxSumPairWithDifferenceLessThanK(int arr[], int N, int K)
+    {
+        // Your code goes here
+        sort(arr,arr+N);
+        
+        int sum = 0;
+        for(int i=N-1;i>0;i--){
+            if(arr[i]-arr[i-1]<K){
+                sum += arr[i]+arr[i-1];
+                i--;
+            }
+        }
+        return sum;
+    }
